7f040da49ebf6013c2741d6448369a4c790dc25b
syneva-runyan <srunyan3@gatech.edu>
2025-03-29 09:38:01 -0800

    minor mobile updats

--- src/components/BusinessIncentive.js
+++ src/components/BusinessIncentive.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBullhorn, faPlug, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 
 function BusinessIncentiveSection() {
@@ -7,11 +9,16 @@ function BusinessIncentiveSection() {
       <h2>Participating Businesses Get..</h2>
       <ul>
         <li>
+          <FontAwesomeIcon icon={faBullhorn} />
           highlighted on tours
         </li>
         <li>
+          <FontAwesomeIcon icon={faPlug} />
           have a direct channel to visitors</li>
-        <li>to showcase their company’s story and connection to the community </li>
+        <li>
+          <FontAwesomeIcon icon={faHeart} />
+          to showcase their company’s story and connection to the community
+          </li>
       </ul>
     </section>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 // src/components/Footer.js
+import './Footer.css';
 import React from 'react';
 
 function Footer() {
   return (
-    <section>
-      <p>© Local Loop LLC, {new Date().getFullYear()}</p>
-      <p>Questions or suggestions? Contact <a href="mailTo:localloop907@gmail.com" target='_blank' rel='noreferrer noopener'>localloop907@gmail.com</a></p>
+    <section className="footer">
+      <p className='footerText'>© Local Loop LLC, {new Date().getFullYear()}</p>
+      <p className='footerText'>Questions or suggestions? Contact <a href="mailTo:localloop907@gmail.com" target='_blank' rel='noreferrer noopener'>localloop907@gmail.com</a></p>
     </section>
   );
 }
