c060b2e577e31ad4852b41ee3f6e124f319e4352
syneva-runyan <srunyan3@gatech.edu>
2025-04-14 08:48:08 -0800

    shorten second question

--- src/components/FunctionalApp/HowMuchTImeDoYouHave.js
+++ src/components/FunctionalApp/HowMuchTImeDoYouHave.js
@@ -24,7 +24,7 @@ function HowMuchTimeDoYouHave() {
     }
     return (
         <div className="tourInputSection howMuchTimeDoYouHave">
-            <p className="howMuchTimeDoYouHaveQuestion primaryQuestion">How long do you have to explore?</p>
+            <p className="howMuchTimeDoYouHaveQuestion primaryQuestion">How long do you have?</p>
             <div className='howMuchTimeDoYouHaveInputs'>
                 <div className="durationInput">
                     <input type="number" id="hours" name="hours" value={hours} onChange={validateAndSetHours} />
