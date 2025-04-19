7e84322d51808224da48184a17fbf84527e10921
syneva-runyan <srunyan3@gatech.edu>
2025-04-19 12:20:54 -0800

    add a botique condition

--- api/get-itinerary/index.mjs
+++ api/get-itinerary/index.mjs
@@ -117,6 +117,7 @@ async function getTourItinerary(parameters) {
           content: `
                 I'm visiting ${parameters.location} Create a personalized tour itinerary for me and sell me on why I should go on it. 
                 Tour should be walkable and fit within ${parameters.hours} hours and ${parameters.minutes} - account for the time it take to walk between stops.
+                Don't scheduled more than 20 minutes at any specific botique.
                 I'm particularly interested in ${parameters.vibes}. Do not choose stops that take more than 20 minutes to walk to from previous stop.
                 Details about the stop should include a paragraph or two of interesting background about the shop, partiularly featuring history. 
                 DO NOT MAKE THINGS UP and include citation urls.
