cecae4762f50ed0b4fae511f9c4a20e6af7888dc
syneva-runyan <srunyan3@gatech.edu>
2025-04-20 15:43:58 -0800

    add details feedback form

--- src/components/FunctionalApp/Tour/Done.js
+++ src/components/FunctionalApp/Tour/Done.js
@@ -5,7 +5,7 @@ const FeedbackButtons = ({ onFeedback }) => {
   return (
     <div>
       <button
-        onClick={() => onFeedback("up")}
+        onClick={() => onFeedback(true)}
         className="thumbsUp feedbackButton"
         aria-label="Thumbs up"
       >
@@ -13,7 +13,7 @@ const FeedbackButtons = ({ onFeedback }) => {
       </button>
 
       <button
-        onClick={() => onFeedback("down")}
+        onClick={() => onFeedback(false)}
         className="thumbsDown feedbackButton"
         aria-label="Thumbs down"
       >
@@ -23,18 +23,55 @@ const FeedbackButtons = ({ onFeedback }) => {
   );
 };
 
+const Details = ({ isPositive, onSubmit }) => {
+  return (
+    <form className='detailedFeedbackContainer' onSubmit={onSubmit}>
+      <label htmlFor="detafeedbackDetailsils">{isPositive ? "What did you like?" : "What could we improve?"}</label>
+      <textarea id="feedbackDetails" className='detailedFeedbackContainerInput' />
+      <button className="submitFeedbackButton button">Submit</button>
+    </form>
+  )
+}
+
+
+function FeedbackForm({ setFeedbackSumbited }) {
+  const [thumbSelected, setThumbSelected] = useState(null);
+  
+  const onThumbClick = (isPositive) => {
+      console.log(`User feedback: ${isPositive}`);
+      setThumbSelected(isPositive);
+  };
+
+  const onSubmit = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      const feedbackDetails = e.target.elements.feedbackDetails.value;
+      console.log(`Feedback details: ${feedbackDetails}`);
+      setFeedbackSumbited(true);
+      // Here you would typically send the feedback to your server
+  };
+
+  return (
+    <>
+     <p><strong>Let us know what you thought!</strong></p>
+      { thumbSelected !== null ?
+        <Details isPositive={thumbSelected} onSubmit={onSubmit} />  :
+      <FeedbackButtons onFeedback={onThumbClick} />
+    }
+    </>
+  )
+}
 
 function Done() {
-    const onFeedback = (feedback) => {
-        console.log(`User feedback: ${feedback}`);
-    };
+  const [feedbackSumbited, setFeedbackSumbited] = useState(false);
+
     return (
         <div className="tour-complete-container">
             <div className="walker">🚶‍♀️</div>
             <h1 className="complete-text">You Finished Your Tour!</h1>
             <div className="confetti"></div>
-            <p><strong>Let us know what you thought!</strong></p>
-            <FeedbackButtons onFeedback={onFeedback} />
+
+            {feedbackSumbited ? <p>Thank you for your feedback!</p>: <FeedbackForm setFeedbackSumbited={setFeedbackSumbited} />}
         </div>
     );
 }
