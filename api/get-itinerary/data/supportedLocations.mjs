4f139e436967ec0956ff82793869e5269bb02aff
syneva-runyan <srunyan3@gatech.edu>
2025-04-27 18:35:26 -0800

    remove perseverance trail from exclude list

--- api/get-itinerary/data/supportedLocations.mjs
+++ api/get-itinerary/data/supportedLocations.mjs
@@ -1,7 +1,7 @@
 const supportedLocations = {
     "Downtown Juneau, Alaska": {
         googlePlaceId: "ChIJU8srZ53fAFQRrrxJtoUYdjw",
-        exclude: "Perseverance Theater, Sandy Beach, Perseverance Trail head"
+        exclude: "Perseverance Theater, Sandy Beach"
     },
 }
 
