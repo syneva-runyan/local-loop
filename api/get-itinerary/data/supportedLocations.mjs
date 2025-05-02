0d76addf918077bba56d8ad3f3bdb7d0b06905af
Syneva <syneva@gmail.com>
2025-05-02 12:22:19 -0800

    Revert "Ground answers in google search"

--- api/get-itinerary/data/supportedLocations.mjs
+++ api/get-itinerary/data/supportedLocations.mjs
@@ -1,9 +1,7 @@
 const supportedLocations = {
     "Downtown Juneau, Alaska": {
         googlePlaceId: "ChIJU8srZ53fAFQRrrxJtoUYdjw",
-        exclude: "Perseverance Theater, Sandy Beach, Rainforest Botique, Alaskan Distillery",
-        latitude: "58.3019W",
-        longitude: "-134.4197N",
+        exclude: "Perseverance Theater, Sandy Beach, Rainforest Botique, Alaskan Distillery"
     },
 }
 
